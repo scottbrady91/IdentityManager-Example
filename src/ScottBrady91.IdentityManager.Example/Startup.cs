0f2f2e105ce8c2cb5414b9fb3e95c5bc1cb42f0f
Scott Brady <scott.brady@rocksolidknowledge.com>
2016-04-07 21:25:33 +0100

    Updated to use factory pattern. Updated packages

--- src/ScottBrady91.IdentityManager.Example/Startup.cs
+++ src/ScottBrady91.IdentityManager.Example/Startup.cs
@@ -1,30 +1,38 @@
-﻿namespace ScottBrady91.IdentityManager.Example
+﻿using IdentityManager;
+using IdentityManager.AspNetIdentity;
+using IdentityManager.Configuration;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Owin;
+
+namespace ScottBrady91.IdentityManager.Example
 {
-    using global::IdentityManager;
-    using global::IdentityManager.AspNetIdentity;
-    using global::IdentityManager.Configuration;
-
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
-
-    using Owin;
-
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             var factory = new IdentityManagerServiceFactory();
+            factory.IdentityManagerService = new Registration<IIdentityManagerService>(Create());
 
-            factory.Register(new Registration<IdentityDbContext>());
-            factory.Register(new Registration<UserStore<IdentityUser>>());
-            factory.Register(new Registration<RoleStore<IdentityRole>>());
-            factory.Register(new Registration<UserManager<IdentityUser,string>>(x => new UserManager<IdentityUser>(x.Resolve<UserStore<IdentityUser>>())));
-            factory.Register(new Registration<RoleManager<IdentityRole,string>>(x => new RoleManager<IdentityRole>(x.Resolve<RoleStore<IdentityRole>>())));
-            
-            factory.IdentityManagerService =
-                new Registration<IIdentityManagerService, AspNetIdentityManagerService<IdentityUser, string, IdentityRole, string>>();
-            
-            app.UseIdentityManager(new IdentityManagerOptions { Factory = factory });
+            app.UseIdentityManager(new IdentityManagerOptions {Factory = factory});
+        }
+
+        private IIdentityManagerService Create()
+        {
+            var context =
+                new IdentityDbContext(
+                    @"Data Source=.\SQLEXPRESS;Initial Catalog=AspIdentity;Integrated Security=true");
+
+            var userStore = new UserStore<IdentityUser>(context);
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            var managerService =
+                new AspNetIdentityManagerService<IdentityUser, string, IdentityRole, string>(userManager, roleManager);
+
+            return managerService;
         }
     }
 }
